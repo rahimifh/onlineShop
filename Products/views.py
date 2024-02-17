82604aafe587b0e5568e133969080161f5add3cb
siavash rahimi <rahimi.f.h@gmail.com>
2024-02-17 23:55:28 +0330

    feat(detail) add detail page to project

--- Products/views.py
+++ Products/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import product
 # Create your views here.
 
-def hello(request):
-    
-    context = {"name": ['siavash', 'mohamad','ali']}
+def home(request):
+    products = product.objects.all()
+    context = {"products":products}
     return render(request,'core/home.html', context)
+
+def pro_detail(request, id):
+    pro = product.objects.get(id = id)
+    print("*********")
+    print(pro.name)
+    context = {"product":pro}
+    return render(request, "core/detail.html", context)
